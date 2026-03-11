206b37e1941d25ed37ec7fd3218f3f665766c2d4
Noxus0685 <53409785+Noxus0685@users.noreply.github.com>
2026-03-11 08:49:49 +0300

    Fix device defaults, type switching, and conditional persistence

--- fuMain.pas
+++ fuMain.pas
@@ -612,7 +612,7 @@ type
     FNextClimateChangeAt: TDateTime;
     procedure UpdateRandomClimate(const AWorkTable: TWorkTable);
     procedure UpdateRandomSignals(const AWorkTable: TWorkTable);
-    procedure OpenTypeSelect(ARow: Integer);
+    procedure OpenTypeSelect(ARow: Integer; const AIsEtalon: Boolean = False);
     procedure OpenChannelDeviceEditor(AChannel: TChannel);
     procedure SelectDeviceForChannel(AChannel: TChannel);
     procedure InitTables;
@@ -4114,7 +4114,7 @@ begin
  // FFlowMeterRows[ARow].Meter.SerialNumber := CFlowMeterSerials[FFlowMeterRows[ARow].SerialIndex];
 end;
 
-procedure TFormMain.OpenTypeSelect(ARow: Integer);
+procedure TFormMain.OpenTypeSelect(ARow: Integer; const AIsEtalon: Boolean);
 var
   Frm: TFormTypeSelect;
   CurrentType, NewType: TDeviceType;
@@ -4129,13 +4129,24 @@ begin
   if (FActiveWorkTable = nil) then
     Exit;
 
-  if (ARow < 0) or (ARow >= FActiveWorkTable.DeviceChannels.Count) then
-    Exit;
+  if AIsEtalon then
+  begin
+    if (ARow < 0) or (ARow >= FActiveWorkTable.EtalonChannels.Count) then
+      Exit;
+  end
+  else
+  begin
+    if (ARow < 0) or (ARow >= FActiveWorkTable.DeviceChannels.Count) then
+      Exit;
+  end;
 
   if (DataManager = nil) then
     Exit;
 
-  Ch := FActiveWorkTable.DeviceChannels[ARow];
+  if AIsEtalon then
+    Ch := FActiveWorkTable.EtalonChannels[ARow]
+  else
+    Ch := FActiveWorkTable.DeviceChannels[ARow];
   if (Ch = nil) or (Ch.FlowMeter = nil) then
     Exit;
 
@@ -4247,7 +4258,7 @@ begin
       if IsTypeChanged then
       begin
         Ch.FlowMeter.Device.AttachType(NewType, RepoName);
-        Ch.FlowMeter.Device.FillFromType(NewType);
+        Ch.FlowMeter.Device.FillFromType(NewType, True);
       end;
     end;
 
@@ -4485,7 +4496,7 @@ begin
     begin
       GridDevices.EditorMode := False;
       if WorkTable <> nil then
-        OpenTypeSelect(Row);
+        OpenTypeSelect(Row, False);
     end
     else if Column = StringColumnDeviceName1 then
     begin
@@ -4711,7 +4722,7 @@ begin
     begin
       GridEtalons.EditorMode := False;
       if WorkTable <> nil then
-        OpenTypeSelect(Row);
+        OpenTypeSelect(Row, True);
     end
     else if Column = StringColumnEtalonName1 then
     begin
