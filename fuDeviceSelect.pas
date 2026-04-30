404dcdb8c138bf1e8ba8a1d7c429daabc2a3ad73
makspyankov150796-design <maks.pyankov.150796@gmail.com>
2026-04-30 08:53:59 +0300

    Fix device category mapping and multi-device delete flow

--- fuDeviceSelect.pas
+++ fuDeviceSelect.pas
@@ -224,6 +224,7 @@ private
   function UpdateConnection: Boolean;             // смена активного репозитория
   procedure ClearTreeAndGrid;                     // очистка UI при смене репозитория
   function GetSelectedDevices: TObjectList<TDevice>;
+  function GetActiveTreeNode: TTreeViewItem;
 
 public
   { Public declarations }
@@ -793,13 +794,26 @@ end;
 procedure TFormDeviceSelect.aDevicePasteExecute(Sender: TObject);
 var
   NewRows: TObjectList<TDevice>;
+  NewDevice: TDevice;
   I: Integer;
+  SelectedNode: TTreeViewItem;
 begin
   if (ActiveRepo = nil) or (AppServices.DataManager = nil) or (not AppServices.DataManager.HasBufferDevices) then
     Exit;
 
+  SelectedNode := GetActiveTreeNode;
   NewRows := AppServices.DataManager.PasteBufferDevices;
   try
+    // По аналогии с FormTypeSelect:
+    // создаём новый прибор из буфера и, если выбрана ветка назначения,
+    // переопределяем только поля привязки к дереву для НОВОГО экземпляра.
+    for I := 0 to NewRows.Count - 1 do
+    begin
+      NewDevice := NewRows[I];
+      if (SelectedNode <> nil) and (SelectedNode.Tag <> Ord(tnAll)) then
+        AppServices.DataManager.AssignDeviceTreeFields(NewDevice, SelectedNode);
+    end;
+
     ApplyFilter;
     UpdateGridDevices;
 
@@ -813,6 +827,70 @@ begin
   finally
     NewRows.Free;
   end;
+end;
+
+function TFormDeviceSelect.GetActiveTreeNode: TTreeViewItem;
+var
+  BestDepth: Integer;
+  procedure CheckCandidate(const ACandidate: TTreeViewItem);
+  var
+    CurDepth: Integer;
+    Parent: TTreeViewItem;
+    J: Integer;
+    ChildNode: TTreeViewItem;
+  begin
+    if ACandidate = nil then
+      Exit;
+
+    if ACandidate.IsSelected then
+    begin
+      CurDepth := 0;
+      Parent := ACandidate.ParentItem;
+      while Parent <> nil do
+      begin
+        Inc(CurDepth);
+        Parent := Parent.ParentItem;
+      end;
+
+      // При множественном выборе берём наиболее глубокий выбранный узел,
+      // чтобы подветка имела приоритет над родительской веткой.
+      if CurDepth >= BestDepth then
+      begin
+        BestDepth := CurDepth;
+        Result := ACandidate;
+      end;
+    end;
+
+    // Обходим всё дерево рекурсивно, а не только корневые элементы.
+    for J := 0 to ACandidate.Count - 1 do
+      if ACandidate.ItemByIndex(J) is TTreeViewItem then
+      begin
+        ChildNode := TTreeViewItem(ACandidate.ItemByIndex(J));
+        CheckCandidate(ChildNode);
+      end;
+  end;
+var
+  I: Integer;
+  CurDepth: Integer;
+  Parent: TTreeViewItem;
+begin
+  Result := TreeViewDevices.Selected;
+  BestDepth := -1;
+
+  if Result <> nil then
+  begin
+    CurDepth := 0;
+    Parent := Result.ParentItem;
+    while Parent <> nil do
+    begin
+      Inc(CurDepth);
+      Parent := Parent.ParentItem;
+    end;
+    BestDepth := CurDepth;
+  end;
+
+  for I := 0 to TreeViewDevices.Count - 1 do
+    CheckCandidate(TreeViewDevices.ItemByIndex(I));
 end;
 
 procedure TFormDeviceSelect.UpdateDeviceActions(Sender: TObject);
@@ -839,7 +917,6 @@ end;
 function TFormDeviceSelect.GetSelectedDevices: TObjectList<TDevice>;
 var
   SelectedDevice: TDevice;
-  I: Integer;
 begin
   Result := TObjectList<TDevice>.Create(False);
 
@@ -852,9 +929,6 @@ begin
 
   if FDevFilteredDevices = nil then
     Exit;
-
-  for I := 0 to FDevFilteredDevices.Count - 1 do
-    Result.Add(FDevFilteredDevices[I]);
 end;
 
 procedure TFormDeviceSelect.ButtonDeviceClearClick(Sender: TObject);
@@ -902,8 +976,9 @@ end;
 
 procedure TFormDeviceSelect.ButtonDeviceDeleteClick(Sender: TObject);
 var
-  SelRow: Integer;
+  TargetDevices: TObjectList<TDevice>;
   SelDevice: TDevice;
+  I: Integer;
 begin
   {----------------------------------}
   { Проверка списка }
@@ -911,51 +986,66 @@ begin
   if (FDevFilteredDevices = nil) or (FDevFilteredDevices.Count = 0) then
     Exit;
 
-  SelRow := GridDevices.Row;
-  if (SelRow < 0) or (SelRow >= FDevFilteredDevices.Count) then
-    Exit;
-
-  { Явно подсвечиваем строку для удаления }
-  GridDevices.Row := SelRow;
-
-  SelDevice := FDevFilteredDevices[SelRow];
-  if SelDevice = nil then
-    Exit;
-
-  {----------------------------------}
-  { Подтверждение удаления }
-  {----------------------------------}
-  if not FSkipDeviceDeleteConfirm then
-  begin
-    if MessageDlg(
-         'Удалить выбранный прибор безвозвратно?',
-         TMsgDlgType.mtWarning,
-         [TMsgDlgBtn.mbYes, TMsgDlgBtn.mbNo],
-         0
-       ) <> mrYes then
+  TargetDevices := GetSelectedDevices;
+  try
+    if TargetDevices.Count = 0 then
       Exit;
 
-    FSkipDeviceDeleteConfirm := True;
+    {----------------------------------}
+    { Подтверждение удаления }
+    {----------------------------------}
+    if not FSkipDeviceDeleteConfirm then
+    begin
+      if TargetDevices.Count = 1 then
+      begin
+        if MessageDlg(
+             'Удалить выбранный прибор безвозвратно?',
+             TMsgDlgType.mtWarning,
+             [TMsgDlgBtn.mbYes, TMsgDlgBtn.mbNo],
+             0
+           ) <> mrYes then
+          Exit;
+      end
+      else
+      begin
+        if MessageDlg(
+             'Удалить выбранные приборы безвозвратно?',
+             TMsgDlgType.mtWarning,
+             [TMsgDlgBtn.mbYes, TMsgDlgBtn.mbNo],
+             0
+           ) <> mrYes then
+          Exit;
+      end;
+
+      FSkipDeviceDeleteConfirm := True;
+    end;
+
+    {----------------------------------}
+    { Удаление через репозиторий }
+    {----------------------------------}
+    for I := TargetDevices.Count - 1 downto 0 do
+    begin
+      SelDevice := TargetDevices[I];
+      if SelDevice <> nil then
+        ActiveRepo.DeleteDevice(SelDevice);
+    end;
+
+    {----------------------------------}
+    { Пересборка фильтров }
+    {----------------------------------}
+    FreeAndNil(FDevFilteredByTree);
+    FDevFilteredByTree := BuildFilteredByTree(FDevices);
+
+    ApplyFilter;
+    UpdateGridDevices;
+
+    {----------------------------------}
+    { Сброс выделения }
+    {----------------------------------}
+    GridDevices.Row := -1;
+  finally
+    TargetDevices.Free;
   end;
-
-  {----------------------------------}
-  { Удаление через репозиторий }
-  {----------------------------------}
-  ActiveRepo.DeleteDevice(SelDevice);
-
-  {----------------------------------}
-  { Пересборка фильтров }
-  {----------------------------------}
-  FreeAndNil(FDevFilteredByTree);
-  FDevFilteredByTree := BuildFilteredByTree(FDevices);
-
-  ApplyFilter;
-  UpdateGridDevices;
-
-  {----------------------------------}
-  { Сброс выделения }
-  {----------------------------------}
-  GridDevices.Row := -1;
 end;
 
 procedure TFormDeviceSelect.ApplyFilter;
