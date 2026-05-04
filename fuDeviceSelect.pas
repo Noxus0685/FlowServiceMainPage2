fa76ede3ca4dc77dccb4db827c9fe5944e6a4006
makspyankov150796-design <maks.pyankov.150796@gmail.com>
2026-05-04 15:41:46 +0300

    Align initial tree/grid selection to first device on form create

--- fuDeviceSelect.pas
+++ fuDeviceSelect.pas
@@ -1963,6 +1963,7 @@ var
   Node: TTreeViewItem;
   ParentNode: TTreeViewItem;
   I: Integer;
+  FoundRow: Boolean;
 begin
   BuildTree;
   Node := FindDeviceTreeNode(ADevice);
@@ -1979,8 +1980,8 @@ begin
       ParentNode := ParentNode.ParentItem;
     end;
 
+    Node.IsSelected := True;
     TreeViewDevices.Selected := Node;
-    TreeViewDevices.SetFocus;
   end;
 
   FreeAndNil(FDevFilteredByTree);
@@ -1995,12 +1996,32 @@ begin
   if FDevFilteredDevices = nil then
     Exit;
 
+  FoundRow := False;
   for I := 0 to FDevFilteredDevices.Count - 1 do
     if FDevFilteredDevices[I] = ADevice then
     begin
       GridDevices.Row := I;
+      GridDevices.Selected := I;
+      FoundRow := True;
       Break;
     end;
+
+  if FoundRow then
+    TThread.ForceQueue(nil,
+      procedure
+      begin
+        if (GridDevices <> nil) and GridDevices.Visible then
+        begin
+          GridDevices.SetFocus;
+          if not GridDevices.IsFocused then
+            TThread.ForceQueue(nil,
+              procedure
+              begin
+                if (GridDevices <> nil) and GridDevices.Visible then
+                  GridDevices.SetFocus;
+              end);
+        end;
+      end);
 
 end;
 
@@ -2340,6 +2361,9 @@ begin
     BuildTree;
     ApplyFilter;
     UpdateGridDevices;
+
+    if (FDevFilteredDevices <> nil) and (FDevFilteredDevices.Count > 0) then
+      SelectEditedDevice(FDevFilteredDevices[0]);
   end;
 end;
 
