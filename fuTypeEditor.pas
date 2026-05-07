e822b0c366194d428812c82dce7869ae1aead03c
makspyankov150796-design <maks.pyankov.150796@gmail.com>
2026-05-07 13:29:22 +0300

    Preserve Qnom/Qmax linkage when editing without flow rate

--- fuTypeEditor.pas
+++ fuTypeEditor.pas
@@ -561,7 +561,7 @@ begin
 
   QmaxForCoef := DCoef.Qmax;
   if (QmaxForCoef <= 0) and (DCoef.Qnom > 0) then
-    QmaxForCoef := DCoef.Qnom / C_QOVER_TO_QMAX;
+    QmaxForCoef := DCoef.Qnom * C_QOVER_TO_QMAX;
 
   if QmaxForCoef > 0 then
   begin
@@ -584,7 +584,7 @@ begin
   else if KnownCol = StringColumnDNQmax.Index then
     Qmax := KnownValue
   else if KnownCol = StringColumnDNQnom.Index then
-    Qmax := KnownValue / C_QOVER_TO_QMAX
+    Qmax := KnownValue * C_QOVER_TO_QMAX
   else
     Exit;
 
@@ -593,7 +593,7 @@ begin
 
   Q2 := Qmax * K2;
   Qmin := Qmax * K1;
-  QOver := Qmax * C_QOVER_TO_QMAX;
+  QOver := Qmax / C_QOVER_TO_QMAX;
 
   ANewD.Qmax := Qmax;
   ANewD.Q2 := Q2;
@@ -976,7 +976,7 @@ begin
       if D.Qnom <= 0 then
       begin
         if D.Qmax > 0 then
-          D.Qnom := D.Qmax * 1.25
+          D.Qnom := D.Qmax / 1.25
         else
           D.Qnom := 0;
       end;
@@ -986,7 +986,7 @@ begin
 
       if (D.State <> osNew) and (D.Qmax > 0) then
       begin
-        D.Qnom := D.Qmax * 1.25;
+        D.Qnom := D.Qmax / 1.25;
         RecalcQRowFromKnown(D, StringColumnDNQmax.Index, D.Qmax);
       end;
       Inc(VisibleCount);
@@ -1330,7 +1330,7 @@ begin
 
     NewD.Q2 := 0;
     NewD.Qmin := 0;
-    NewD.Qnom := NewD.Qmax * 1.25;
+    NewD.Qnom := NewD.Qmax / 1.25;
     RecalcQRowFromKnown(NewD, StringColumnDNQmax.Index, NewD.Qmax);
   end;
 
@@ -3450,16 +3450,24 @@ begin
 
     if Trim(EditFlowRate.Text) = '' then
     begin
-      // Если скорость потока не задана, меняем только редактируемое поле,
-      // без пересчёта остальных Q-столбцов.
+      // Если скорость потока не задана, меняем редактируемое поле.
+      // Для пары Qnom/Qmax сохраняем взаимосвязь по формулам 1.25.
       if ACol = StringColumnDNQTr.Index then
         D.Q2 := QValueBase
       else if ACol = StringColumnDNQmax.Index then
-        D.Qmax := QValueBase
+      begin
+        D.Qmax := QValueBase;
+        if D.Qmax > 0 then
+          D.Qnom := D.Qmax / 1.25;
+      end
       else if ACol = StringColumnDNQmin.Index then
         D.Qmin := QValueBase
       else if ACol = StringColumnDNQnom.Index then
+      begin
         D.Qnom := QValueBase;
+        if D.Qnom > 0 then
+          D.Qmax := D.Qnom * 1.25;
+      end;
     end
     else
     begin
