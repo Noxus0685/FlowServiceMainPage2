7589c6284af7b6697ec64b55ca8694586cea1d79
makspyankov150796-design <maks.pyankov.150796@gmail.com>
2026-05-14 08:49:25 +0300

    Revert "Prevent duplicate UUIDs on Arshin import and fix CreateDevice usage"

--- fuDeviceSelect.pas
+++ fuDeviceSelect.pas
@@ -909,7 +909,7 @@ begin
      (SelRow < FDevFilteredDevices.Count) then
     SrcDevice := FDevFilteredDevices[SelRow];
 
-  NewDevice := ActiveRepo.CreateDevice(SrcDevice);
+  ActiveRepo.CreateDevice(SrcDevice);
 
 
 
@@ -1518,7 +1518,6 @@ var
   FoundCount: Integer;
 
   Dev: TDevice;
-  ExistingDevice: TDevice;
   FoundType: TDeviceType;
   FoundRepo: TTypeRepository;
 
@@ -1528,7 +1527,6 @@ var
   ImportedModification: string;
   ImportedSerialNumber: string;
   ImportedOwner: string;
-  ImportedUUID: string;
   ImportedRegDate: TDate;
   ImportedValidityDate: TDate;
   ImportedDocNum: string;
@@ -1673,17 +1671,7 @@ begin
           Dev := ActiveRepo.CreateDevice(-1);
 
           if Item.GetValue('vri_id') <> nil then
-          begin
-            ImportedUUID := Trim(Item.GetValue('vri_id').Value);
-            ExistingDevice := ActiveRepo.FindDeviceByUUID(ImportedUUID);
-            if (ImportedUUID <> '') and (ExistingDevice <> nil) then
-            begin
-              ActiveRepo.DeleteDevice(Dev);
-              Dev := ExistingDevice;
-            end
-            else if ImportedUUID <> '' then
-              Dev.UUID := ImportedUUID;
-          end;
+            Dev.UUID := Item.GetValue('vri_id').Value;
 
           ImportedOwner := '';
           ImportedReestr := '';
--- fuTypeSelect.pas
+++ fuTypeSelect.pas
@@ -2126,11 +2126,9 @@ var
 
   I: Integer;
   DevType: TDeviceType;
-  ExistingType: TDeviceType;
 
   SearchText: string;
   DetectText: string;
-  ImportedUUID: string;
   TotalCount: Integer;
 begin
   {----------------------------------}
@@ -2233,17 +2231,7 @@ if MessageDlg(
 
         {------------ mit_uuid ------------}
         if Item.GetValue('mit_uuid') <> nil then
-        begin
-          ImportedUUID := Trim(Item.GetValue('mit_uuid').Value);
-          ExistingType := ActiveRepo.FindTypeByUUID(ImportedUUID);
-          if (ImportedUUID <> '') and (ExistingType <> nil) then
-          begin
-            ActiveRepo.DeleteType(DevType);
-            DevType := ExistingType;
-          end
-          else if ImportedUUID <> '' then
-            DevType.UUID := ImportedUUID;
-        end;
+          DevType.UUID := Item.GetValue('mit_uuid').Value;
 
         {------------ ГРСИ ------------}
         if Item.GetValue('number') <> nil then
