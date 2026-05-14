8e392794180f7e39d342ac1034eb43891d06ed7d
makspyankov150796-design <maks.pyankov.150796@gmail.com>
2026-05-14 15:36:38 +0300

    Добавить экспорт журнала в txt и вернуть расширенные поля логов

--- fuDeviceEdit.pas
+++ fuDeviceEdit.pas
@@ -41,7 +41,8 @@ uses
   uClasses,
   uDataManager,
   uDeviceClass,
-  uRepositories;
+  uRepositories,
+  uProtocols;
 
 type
   TFormDeviceEditor = class(TForm)
@@ -306,6 +307,7 @@ type
     { Private declarations }
      FDevice: TDevice;
      FOriginalDevice: TDevice;
+     FInitialTypeUUID: string;
 
 
      FDeviceType: TDeviceType; // ссылка на найденный тип
@@ -371,6 +373,7 @@ type
   public
     { Public declarations }
      procedure LoadDevice(ADevice: TDevice);
+     procedure WriteDeviceEditActionLog(const AAction: string; ADevice: TDevice; const ADetails: string = '');
   end;
 
 var
@@ -640,6 +643,19 @@ begin
   FillConversionCoefVolume;
 end;
 
+
+procedure TFormDeviceEditor.WriteDeviceEditActionLog(const AAction: string; ADevice: TDevice; const ADetails: string);
+var
+  Details: string;
+begin
+  if (ADevice = nil) or (ProtocolManager = nil) then Exit;
+  Details := Format('Action=%s; Form=%s; Object=%s; UUID=%s; Name=%s; Serial=%s; TypeUUID=%s; TypeName=%s; Time=%s',
+    [AAction, 'fuDeviceEdit', 'Device', string(ADevice.UUID), ADevice.Name, ADevice.SerialNumber,
+     string(ADevice.DeviceTypeUUID), ADevice.DeviceTypeName, FormatDateTime('dd.mm.yyyy hh:nn:ss', Now)]);
+  if Trim(ADetails) <> '' then Details := Details + '; ' + ADetails;
+  ProtocolManager.AddMessage(pcInfo, psForm, 'DeviceAction', 'Действие с прибором', Details);
+end;
+
 procedure TFormDeviceEditor.btnCancelClick(Sender: TObject);
 begin
   // Отменяем все изменения
@@ -653,7 +669,8 @@ begin
       AppServices.DataManager.ActiveDeviceRepo.Load;    // предполагается, что у FDevice есть метод для отката изменений
 
       // Закрываем форму с результатом Cancel
-      ModalResult := mrCancel;
+      WriteDeviceEditActionLog('Редактирование прибора отменено', FDevice);
+    ModalResult := mrCancel;
     end;
   end
   else
@@ -901,12 +918,18 @@ begin
         { редактирование существующего }
         FOriginalDevice.Assign(FDevice,True);
         AppServices.DataManager.ActiveDeviceRepo.SaveDevice(FOriginalDevice);
+        WriteDeviceEditActionLog('Сохранён прибор', FOriginalDevice);
       end
       else
       begin
         { новый прибор }
         AppServices.DataManager.ActiveDeviceRepo.SaveDevice(FDevice);
+        WriteDeviceEditActionLog('Сохранён прибор', FDevice);
       end;
+
+      if not SameText(FInitialTypeUUID, string(FDevice.DeviceTypeUUID)) then
+        WriteDeviceEditActionLog('Изменен тип прибора', FDevice,
+          'OldTypeUUID=' + FInitialTypeUUID + '; NewTypeUUID=' + string(FDevice.DeviceTypeUUID));
     end
     else if ModalResult = mrCancel then
     begin
@@ -1390,6 +1413,7 @@ begin
       FOriginalDevice := ADevice;
       //Создаем новый прибор в новой области памяти идентичный данному.
       FDevice := ADevice.Clone;
+      FInitialTypeUUID := string(ADevice.DeviceTypeUUID);
     end
     else
     begin
@@ -1401,6 +1425,7 @@ begin
         FDevice := AppServices.DataManager.ActiveDeviceRepo.CreateDevice(0)
       else
         FDevice := TDevice.Create;
+      FInitialTypeUUID := string(FDevice.DeviceTypeUUID);
     end;
 
     {----------------------------------}
@@ -1529,6 +1554,7 @@ begin
     if Frm.ShowModal <> mrOk then
       Exit;
 
+    OldTypeUUID := FDevice.DeviceTypeUUID;
     NewType := Frm.SelectedType;
     if NewType = nil then
       Exit;
@@ -1570,6 +1596,8 @@ begin
   begin
     FDevice.AttachType(NewType, RepoName);
     FDeviceType := NewType;
+    WriteDeviceEditActionLog('Изменён тип прибора', FDevice,
+      'OldTypeUUID=' + OldTypeUUID + '; NewTypeUUID=' + string(FDevice.DeviceTypeUUID));
   end;
 
 
