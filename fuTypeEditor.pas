9ed17aa3a4c84e2342530f5f80b579fd14f65e91
makspyankov150796-design <maks.pyankov.150796@gmail.com>
2026-05-19 13:29:19 +0300

    Revert "Pass modification to DeepSeek template, auto-hide empty diameter columns, and improve PDF/DeepSeek handling"

--- fuTypeEditor.pas
+++ fuTypeEditor.pas
@@ -1222,10 +1222,6 @@ begin
   if (FDiametersLocal = nil) or (GridDiameters = nil) then
     Exit;
 
-  // Если в гриде нет строк, видимость столбцов не меняем.
-  if GridDiameters.RowCount <= 0 then
-    Exit;
-
   EnsureUniqueDiameterIDs;
 
   PrevRow := GridDiameters.Row;
@@ -1271,47 +1267,6 @@ begin
   finally
     GridDiameters.EndUpdate;
   end;
-
-end;
-
-procedure TFormTypeEditor.AutoHideEmptyDiameterColumns;
-var
-  D: TDiameter;
-  HasName, HasQnom, HasQtr, HasQ2tr, HasKp, HasQf: Boolean;
-  function HasNonZeroValue(const AValue: Double): Boolean;
-  begin
-    Result := not SameValue(AValue, 0, MinDouble);
-  end;
-begin
-  if (FDiametersLocal = nil) or (GridDiameters = nil) then
-    Exit;
-
-  HasName := False;
-  HasQnom := False;
-  HasQtr := False;
-  HasQ2tr := False;
-  HasKp := False;
-  HasQf := False;
-
-  for D in FDiametersLocal do
-    if (D <> nil) and (D.State <> osDeleted) then
-    begin
-      HasName := HasName or ((Trim(D.Name) <> '') and (Trim(D.Name) <> '-'));
-      HasQnom := HasQnom or HasNonZeroValue(D.Qnom);
-      HasQtr := HasQtr or HasNonZeroValue(D.Qtr);
-      HasQ2tr := HasQ2tr or HasNonZeroValue(D.Q2tr);
-      HasKp := HasKp or HasNonZeroValue(D.Kp);
-      HasQf := HasQf or HasNonZeroValue(D.QFmax);
-    end;
-
-  StringColumnDNName.Visible := HasName;
-  StringColumnDNQnom.Visible := HasQnom;
-  StringColumnDNQTr.Visible := HasQtr;
-  StringColumnDNQ2tr.Visible := HasQ2tr;
-  StringColumnDNKp.Visible := HasKp;
-  StringColumnDNQF.Visible := HasQf;
-  SyncGridDiametersHeaderPopupMenu;
-  UpdateGridDiametersHeaderRect;
 end;
 
 procedure TFormTypeEditor.AutoHideEmptyDiameterColumns;
