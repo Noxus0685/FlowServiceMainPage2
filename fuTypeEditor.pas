56327e8dc871373c415f246c3c19947c2f7b9c0b
makspyankov150796-design <maks.pyankov.150796@gmail.com>
2026-05-19 16:58:30 +0300

    Normalize tree keys to avoid case-sensitive manufacturer branches

--- fuTypeEditor.pas
+++ fuTypeEditor.pas
@@ -253,7 +253,6 @@ type
     MemoLog: TMemo;
     NetHTTPClient1: TNetHTTPClient;
     DeepSeek: TSpeedButton;
-    btnLoadLocalJson: TSpeedButton;
     TabControlMain: TTabControl;
     TabItemDevice: TTabItem;
     TabItemCoefs: TTabItem;
@@ -356,7 +355,6 @@ type
     procedure ceCategoryChange(Sender: TObject);
     procedure sbFindReestrNumberClick(Sender: TObject);
     procedure DeepSeekClick(Sender: TObject);
-    procedure btnLoadLocalJsonClick(Sender: TObject);
     procedure ChatGPTClick(Sender: TObject);
     procedure AddFileClick(Sender: TObject);
     procedure SpeedButton2Click(Sender: TObject);
@@ -1274,45 +1272,6 @@ begin
   end;
   GridDiameters.Repaint;
 
-end;
-
-procedure TFormTypeEditor.btnLoadLocalJsonClick(Sender: TObject);
-var
-  OpenDialog: TOpenDialog;
-  JsonFilePath: string;
-  JsonText: string;
-begin
-  OpenDialog := TOpenDialog.Create(Self);
-  try
-    OpenDialog.Filter := 'JSON files (*.json)|*.json|Text files (*.txt)|*.txt|All files (*.*)|*.*';
-    OpenDialog.Options := [TOpenOption.ofFileMustExist];
-    OpenDialog.DefaultExt := 'json';
-
-    if not OpenDialog.Execute then
-      Exit;
-
-    JsonFilePath := OpenDialog.FileName;
-  finally
-    OpenDialog.Free;
-  end;
-
-  if not TFile.Exists(JsonFilePath) then
-  begin
-    ShowMessage('Файл не найден: ' + JsonFilePath);
-    Exit;
-  end;
-
-  JsonText := TFile.ReadAllText(JsonFilePath, TEncoding.UTF8);
-  if not ApplyDeepSeekJsonToType(JsonText) then
-  begin
-    ShowMessage('Не удалось применить данные из файла: ' + ExtractFileName(JsonFilePath));
-    Exit;
-  end;
-
-  UpdateUIFromType;
-  MemoLog.Visible := True;
-  MemoLog.Lines.Add('Данные применены из локального файла: ' + ExtractFileName(JsonFilePath));
-  ShowMessage('Данные загружены из файла: ' + ExtractFileName(JsonFilePath));
 end;
 
 procedure TFormTypeEditor.AutoHideEmptyDiameterColumns;
--- uBaseProcedures.pas
+++ uBaseProcedures.pas
@@ -169,6 +169,7 @@ function ExtractManufacturerName(const S: string): string;
 function NormalizeNameCase(const S: string): string;
 function NormalizeSearchText(const S: string): string;
 function NormalizeTreeText(const S: string): string;
+function NormalizeTreeKey(const S: string): string;
 function FindChildInNode(AParent: TTreeViewItem; ATag: Integer; const AKey: string): TTreeViewItem;
 function FindChildInTree(ATree: TTreeView; ATag: Integer; const AKey: string): TTreeViewItem;
 function NewGuidString: string;
@@ -884,6 +885,11 @@ begin
     Result := Trim(S);
 end;
 
+function NormalizeTreeKey(const S: string): string;
+begin
+  Result := UpperCase(Trim(S));
+end;
+
 function FindChildInTree(
   ATree: TTreeView;
   ATag: Integer;
@@ -898,7 +904,7 @@ begin
   for I := 0 to ATree.Count - 1 do
   begin
     Item := ATree.Items[I];
-    if (Item.Tag = ATag) and (Item.TagString = AKey) then
+    if (Item.Tag = ATag) and SameText(Item.TagString, AKey) then
       Exit(Item);
   end;
 end;
@@ -917,7 +923,7 @@ begin
   for I := 0 to AParent.Count - 1 do
   begin
     Item := TTreeViewItem(AParent.Items[I]);
-    if (Item.Tag = ATag) and (Item.TagString = AKey) then
+    if (Item.Tag = ATag) and SameText(Item.TagString, AKey) then
       Exit(Item);
   end;
 end;
