da4c04fd5b08f51a29058c627a478c4bea2b0eb1
makspyankov150796-design <maks.pyankov.150796@gmail.com>
2026-05-20 16:45:02 +0300

    Убрать KeyPreview для совместимости FMX

--- fuDeviceSelect.pas
+++ fuDeviceSelect.pas
@@ -174,6 +174,10 @@ type
     procedure aRefreshRepositoryExecute(Sender: TObject);
     procedure GridDevicesKeyDown(Sender: TObject; var Key: Word; var KeyChar: Char;
       Shift: TShiftState);
+    procedure GridDevicesMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Single);
+    procedure FormKeyDown(Sender: TObject; var Key: Word; var KeyChar: Char;
+      Shift: TShiftState);
 
 private
 
@@ -2450,7 +2454,9 @@ procedure TFormDeviceSelect.FormCreate(Sender: TObject);
 var
   SelectionContext: TDeviceSelectionContext;
 begin
+  OnKeyDown := FormKeyDown;
   GridDevices.OnKeyDown := GridDevicesKeyDown;
+  GridDevices.OnMouseDown := GridDevicesMouseDown;
 
   {----------------------------------}
   { Инициализация сортировки }
@@ -2484,15 +2490,60 @@ begin
   end;
 end;
 
+procedure TFormDeviceSelect.FormKeyDown(Sender: TObject; var Key: Word;
+  var KeyChar: Char; Shift: TShiftState);
+begin
+  if Key = vkEscape then
+  begin
+    ModalResult := mrOk;
+    Key := 0;
+    KeyChar := #0;
+  end;
+end;
+
 procedure TFormDeviceSelect.GridDevicesKeyDown(Sender: TObject; var Key: Word;
   var KeyChar: Char; Shift: TShiftState);
 begin
+  if Key = vkEscape then
+  begin
+    ModalResult := mrOk;
+    Key := 0;
+    KeyChar := #0;
+    Exit;
+  end;
+
+  if Key = vkReturn then
+  begin
+    ModalResult := mrOk;
+    Key := 0;
+    KeyChar := #0;
+    Exit;
+  end;
+
   if Key = vkDelete then
   begin
     ButtonDeviceDeleteClick(ButtonDeviceDelete);
     Key := 0;
     KeyChar := #0;
   end;
+end;
+
+procedure TFormDeviceSelect.GridDevicesMouseDown(Sender: TObject;
+  Button: TMouseButton; Shift: TShiftState; X, Y: Single);
+var
+  Col, Row: Integer;
+begin
+  if Button <> TMouseButton.mbLeft then
+    Exit;
+
+  if not GridDevices.CellByPoint(X, Y, Col, Row) then
+    Exit;
+
+  if (FDevFilteredDevices = nil) or (Row < 0) or (Row >= FDevFilteredDevices.Count) then
+    Exit;
+
+  if ssDouble in Shift then
+    CornerButtonEditDeviceClick(CornerButtonEditDevice);
 end;
 
 procedure TFormDeviceSelect.ApplyInitialSelection;
