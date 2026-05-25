b1ad323da434cc0d86950aca82c80c4693f77e55
makspyankov150796-design <maks.pyankov.150796@gmail.com>
2026-05-25 16:23:27 +0300

    Harden device tree reselection and add Delete hotkey for types

--- fuDeviceSelect.pas
+++ fuDeviceSelect.pas
@@ -1454,7 +1454,7 @@ begin
     Exit;
 
   ParentNode := SelectedNode.ParentItem;
-  ReplacementNode := ParentNode;
+  ReplacementNode := nil;
   if ParentNode <> nil then
   begin
     NodeIndex := -1;
@@ -1499,10 +1499,14 @@ begin
     CurrentNode := ParentNode;
   end;
 
-  if ReplacementNode <> nil then
+  if (ReplacementNode <> nil) and (ReplacementNode.TreeView = TreeViewDevices) then
     TreeViewDevices.Selected := ReplacementNode
+  else if (CurrentNode <> nil) and (CurrentNode.TreeView = TreeViewDevices) then
+    TreeViewDevices.Selected := CurrentNode
+  else if TreeViewDevices.Count > 0 then
+    TreeViewDevices.Selected := TreeViewDevices.ItemByIndex(0)
   else
-    TreeViewDevices.Selected := CurrentNode;
+    TreeViewDevices.Selected := nil;
 end;
 
 procedure TFormDeviceSelect.ApplyFilter;
--- fuTypeSelect.pas
+++ fuTypeSelect.pas
@@ -1111,7 +1111,7 @@ begin
     Exit;
 
   ParentNode := SelectedNode.ParentItem;
-  ReplacementNode := ParentNode;
+  ReplacementNode := nil;
   if ParentNode <> nil then
   begin
     NodeIndex := -1;
@@ -1152,10 +1152,14 @@ begin
     CurrentNode := ParentNode;
   end;
 
-  if ReplacementNode <> nil then
+  if (ReplacementNode <> nil) and (ReplacementNode.TreeView = TreeViewTypes) then
     TreeViewTypes.Selected := ReplacementNode
+  else if (CurrentNode <> nil) and (CurrentNode.TreeView = TreeViewTypes) then
+    TreeViewTypes.Selected := CurrentNode
+  else if TreeViewTypes.Count > 0 then
+    TreeViewTypes.Selected := TreeViewTypes.ItemByIndex(0)
   else
-    TreeViewTypes.Selected := CurrentNode;
+    TreeViewTypes.Selected := nil;
 end;
 
 procedure TFormTypeSelect.RemoveTreeNode(ANode: TTreeViewItem);
@@ -1539,6 +1543,14 @@ begin
   if (Key = vkReturn) and (actTypeSelect.Enabled=true) and (GridTypes.Row>=0) then
   begin
     actTypeSelectExecute(actTypeSelect);
+    Key := 0;
+    KeyChar := #0;
+    Exit;
+  end;
+
+  if (Key = vkDelete) and (GridTypes.Row >= 0) and actTypeDelete.Enabled then
+  begin
+    actTypeDeleteExecute(actTypeDelete);
     Key := 0;
     KeyChar := #0;
   end;
