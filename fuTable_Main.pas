9a55ea60b7ee15b1578fde0a0f5b148c628a8002
makspyankov150796-design <maks.pyankov.150796@gmail.com>
2026-06-01 15:27:26 +0300

    Store active work table name from edit

--- fuTable_Main.pas
+++ fuTable_Main.pas
@@ -81,6 +81,9 @@ type
     procedure  PumpStateHandler(AParameters: TParameter; AAction:EActionParameter);
     procedure EditEtalonFlowRateExit(Sender: TObject);
     procedure EditDeviceFlowRateExit(Sender: TObject);
+    procedure ActiveWorkTableChangeTracking(Sender: TObject);
+    procedure ActiveWorkTableExit(Sender: TObject);
+    procedure ActiveWorkTableKeyDown(Sender: TObject; var Key: Word; var KeyChar: WideChar; Shift: TShiftState);
 
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure TrackStdChange(Sender: TObject);
@@ -98,6 +101,7 @@ type
     FT_WorkBench_First: Double;
     FPrevFlowRateValue: Double;
     FHasPrevFlowRateValue: Boolean;
+    FUpdatingActiveWorkTableEdit: Boolean;
 
     procedure UpdateTemp(const AWorkTable: TWorkTable);
 
@@ -123,6 +127,8 @@ type
     procedure UnsubscribeFromRelatedObjects(const AWorkTable: TWorkTable;
       const AObserver: IEventObserver);
     procedure HandleWorkTableNotify(ASender: TObject; AEvent: EWorkTableNotifyEvent; AData: TObject);
+    procedure UpdateActiveWorkTableEdit(const AForce: Boolean = False);
+    procedure ApplyActiveWorkTableName;
     procedure OnNotify(Sender: TObject; Event: Integer; Data: TObject);
     function QueryInterface(const IID: TGUID; out Obj): HResult; stdcall;
     function _AddRef: Integer; stdcall;
@@ -146,6 +152,55 @@ destructor TTableMainForm.Destroy;
 begin
   UnsubscribeFromWorkTable;
   inherited Destroy;
+end;
+
+procedure TTableMainForm.ActiveWorkTableChangeTracking(Sender: TObject);
+begin
+  ApplyActiveWorkTableName;
+end;
+
+procedure TTableMainForm.ActiveWorkTableExit(Sender: TObject);
+begin
+  ApplyActiveWorkTableName;
+end;
+
+procedure TTableMainForm.ActiveWorkTableKeyDown(Sender: TObject; var Key: Word;
+  var KeyChar: WideChar; Shift: TShiftState);
+begin
+  if Key = vkReturn then
+  begin
+    ApplyActiveWorkTableName;
+    Key := 0;
+  end;
+end;
+
+procedure TTableMainForm.ApplyActiveWorkTableName;
+var
+  WorkTable: TWorkTable;
+  NewName: string;
+begin
+  if FUpdatingActiveWorkTableEdit or (FWorkTableManager = nil) or
+     (ActiveWorkTable = nil) then
+    Exit;
+
+  WorkTable := FWorkTableManager.ActiveWorkTable;
+  if WorkTable = nil then
+    Exit;
+
+  NewName := Trim(ActiveWorkTable.Text);
+  if NewName = '' then
+  begin
+    if not ActiveWorkTable.IsFocused then
+      UpdateActiveWorkTableEdit(True);
+    Exit;
+  end;
+
+  if WorkTable.Name = NewName then
+    Exit;
+
+  WorkTable.Name := NewName;
+  WorkTable.FireEvent(ewtRefresh);
+  FWorkTableManager.Save;
 end;
 
 procedure TTableMainForm.ButtonApplyDeviceValuesClick(Sender: TObject);
@@ -341,6 +396,7 @@ i:integer;
 begin
   FPrevFlowRateValue := 0;
   FHasPrevFlowRateValue := False;
+  FUpdatingActiveWorkTableEdit := False;
 
   //Значения по умолчанию
   FT_WorkBench_First:=20;
@@ -364,6 +420,7 @@ begin
   end;
 
   SubscribeToWorkTable(FWorkTableManager.ActiveWorkTable);
+  UpdateActiveWorkTableEdit;
 
 
   FFrameMainTable := TFrameMainTable.Create(Self);
@@ -569,6 +626,10 @@ EnabledEtalonChannels: TObjectList<TChannel>;
 
      notifyEvent:
       begin
+        if (ASender is TWorkTable) and
+           (TWorkTable(ASender).Event in [Ord(ewtActivated), Ord(ewtRefresh)]) then
+          UpdateActiveWorkTableEdit;
+
         if AData is TPump then
           mPump.Lines.Add('Событие насоса, код: ' + IntToStr(TPump(AData).Event));
         if AData is TFlowRate then
@@ -645,6 +706,36 @@ begin
     FFrameMainTable.UpdateForm;
 end;
 
+procedure TTableMainForm.UpdateActiveWorkTableEdit(const AForce: Boolean);
+var
+  WorkTable: TWorkTable;
+  DisplayName: string;
+begin
+  if (FWorkTableManager = nil) or (ActiveWorkTable = nil) or
+     (ActiveWorkTable.IsFocused and not AForce) then
+    Exit;
+
+  WorkTable := FWorkTableManager.ActiveWorkTable;
+  if WorkTable = nil then
+    DisplayName := ''
+  else
+  begin
+    DisplayName := Trim(WorkTable.Name);
+    if DisplayName = '' then
+      DisplayName := WorkTable.Text;
+  end;
+
+  if ActiveWorkTable.Text = DisplayName then
+    Exit;
+
+  FUpdatingActiveWorkTableEdit := True;
+  try
+    ActiveWorkTable.Text := DisplayName;
+  finally
+    FUpdatingActiveWorkTableEdit := False;
+  end;
+end;
+
 procedure TTableMainForm.UpdateTemp(const AWorkTable: TWorkTable);
 var
   TempDelta, PressDelta: Double; // Случайные приращения температуры и давления
@@ -686,6 +777,17 @@ end;
 
 procedure TTableMainForm.TimerSetValuesTimer(Sender: TObject);
 begin
+  if FWorkTableManager = nil then
+    Exit;
+
+  if FSubscribedWorkTable <> FWorkTableManager.ActiveWorkTable then
+  begin
+    SubscribeToWorkTable(FWorkTableManager.ActiveWorkTable);
+    UpdateActiveWorkTableEdit;
+  end
+  else
+    UpdateActiveWorkTableEdit;
+
   FWorkTableManager.UpdateSimulation;
 end;
 
