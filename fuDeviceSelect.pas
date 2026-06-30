4015bb9e70239eb384593cc82106d929b6c84692
makspyankov150796-design <maks.pyankov.150796@gmail.com>
2026-06-30 10:43:13 +0300

    Avoid reloading device repo on selector open

--- fuDeviceSelect.pas
+++ fuDeviceSelect.pas
@@ -35,6 +35,7 @@ uses
   System.Generics.Collections,
   System.Generics.Defaults,
   System.JSON,
+  System.IOUtils,
   System.Net.HttpClient,
   System.Net.HttpClientComponent,
   System.Net.URLClient,
@@ -274,6 +275,17 @@ implementation
    uAppServices,
    uWorkTable;
 {$R *.fmx}
+
+procedure DbgDeviceSelect(const AText: string);
+var
+  LogFileName: string;
+begin
+  LogFileName := TPath.Combine(ExtractFilePath(ParamStr(0)), 'ProceedTreeDebug.log');
+  TFile.AppendAllText(LogFileName,
+    FormatDateTime('yyyy-mm-dd hh:nn:ss.zzz', Now) + ' ' + AText + sLineBreak + sLineBreak,
+    TEncoding.UTF8);
+end;
+
 constructor TFormDeviceSelect.Create(AOwner: TComponent);
 begin
   inherited;
@@ -358,6 +370,7 @@ end;
 
 procedure TFormDeviceSelect.LoadData;
 begin
+  DbgDeviceSelect('DBG 2001'#13#10'DeviceSelect.LoadData ENTER');
   {--------------------------------------------------}
   { Проверяем наличие активного репозитория приборов }
   {--------------------------------------------------}
@@ -371,10 +384,10 @@ begin
   ActiveRepo := AppServices.DataManager.ActiveDeviceRepo;
 
   {--------------------------------------------------}
-  { Загружаем данные из БД (в репозиторий!) }
+  { Не перезагружаем репозиторий здесь: ActiveRepo.Load/InitChannels
+    меняют состояние рабочего стола при простом открытии DeviceSelect. }
   {--------------------------------------------------}
-  ActiveRepo.Load;
-  WorkTableManager.ActiveWorkTable.InitChannels ;
+  DbgDeviceSelect('DBG 2002'#13#10'DeviceSelect.LoadData uses existing ActiveRepo.Devices');
   {--------------------------------------------------}
   { Берём ссылку на данные репозитория }
   {--------------------------------------------------}
@@ -742,10 +755,13 @@ var
       end;
   end;
 begin
+  DbgDeviceSelect('DBG 2011'#13#10'DeviceSelect.BuildTree ENTER');
   if ActiveRepo = nil then
   begin
+    DbgDeviceSelect('DBG 2012'#13#10'DeviceSelect.BuildTree before TreeViewDevices.Clear');
     TreeViewDevices.Clear;
     GridDevices.RowCount := 0;
+    DbgDeviceSelect('DBG 2013'#13#10'DeviceSelect.BuildTree EXIT');
     Exit;
   end;
 
@@ -966,6 +982,7 @@ begin
 
     for I := 0 to TreeViewDevices.Count - 1 do
       RestoreExpandedNodes(TreeViewDevices.ItemByIndex(I));
+    DbgDeviceSelect('DBG 2013'#13#10'DeviceSelect.BuildTree EXIT');
   finally
     PrevExpandedPaths.Free;
     TreeViewDevices.EndUpdate;
@@ -1080,6 +1097,7 @@ end;
 
 procedure TFormDeviceSelect.CornerButton1Click(Sender: TObject);
 begin
+  DbgDeviceSelect('DBG 2031'#13#10'DeviceSelect OK/select clicked; ModalResult will be mrOk');
   Tag := 1;
   ModalResult := mrOk;
 end;
@@ -1092,6 +1110,7 @@ begin
   if (FDevFilteredDevices = nil) or (GridDevices.Row < 0) or (GridDevices.Row >= FDevFilteredDevices.Count) then
     Exit;
 
+  DbgDeviceSelect('DBG 2031'#13#10'DeviceSelect OK/select clicked; ModalResult will be mrOk');
   Tag := 1;
   ModalResult := mrOk;
 end;
@@ -2589,6 +2608,7 @@ var
   Repo: TDeviceRepository;
   Res: TModalResult;
 begin
+  DbgDeviceSelect('DBG 2021'#13#10'DeviceSelect.FormClose ENTER; ModalResult=' + IntToStr(Ord(ModalResult)));
   Repo := AppServices.DataManager.ActiveDeviceRepo;
 
   if (Repo <> nil) and (Repo.State = osModified) then
@@ -2624,13 +2644,17 @@ begin
           { Откатываем изменения в памяти, чтобы вызывающая форма не считала
             несохранённо удалённые приборы реально удалёнными. }
           FDeletedDeviceUUIDs.Clear;
+          DbgDeviceSelect('DBG 2022'#13#10'DeviceSelect.FormClose before Repo.Load rollback');
           if not Repo.Load then
           begin
             ShowMessage('Не удалось отменить несохранённые изменения приборов');
             Action := TCloseAction.caNone;
             Exit;
           end;
+          DbgDeviceSelect('DBG 2023'#13#10'DeviceSelect.FormClose after Repo.Load rollback');
+          DbgDeviceSelect('DBG 2024'#13#10'DeviceSelect.FormClose before LoadData');
           LoadData;
+          DbgDeviceSelect('DBG 2025'#13#10'DeviceSelect.FormClose after LoadData');
           BuildTree;
           ApplyFilter;
           UpdateGridDevices;
@@ -2690,6 +2714,7 @@ procedure TFormDeviceSelect.FormKeyDown(Sender: TObject; var Key: Word;
 begin
   if (Key = vkEscape) and IsGridInputFocused then
   begin
+    DbgDeviceSelect('DBG 2032'#13#10'DeviceSelect cancel/close clicked');
     ModalResult := mrCancel;
     Key := 0;
     KeyChar := #0;
@@ -2701,6 +2726,7 @@ procedure TFormDeviceSelect.GridDevicesKeyDown(Sender: TObject; var Key: Word;
 begin
   if (Key = vkEscape) and IsGridInputFocused then
   begin
+    DbgDeviceSelect('DBG 2032'#13#10'DeviceSelect cancel/close clicked');
     ModalResult := mrCancel;
     Key := 0;
     KeyChar := #0;
@@ -2709,6 +2735,7 @@ begin
 
   if (Key = vkReturn) and (GridDevices.Row>=0) then
   begin
+    DbgDeviceSelect('DBG 2031'#13#10'DeviceSelect OK/select clicked; ModalResult will be mrOk');
     Tag := 1;
     ModalResult := mrOk;
     Key := 0;
