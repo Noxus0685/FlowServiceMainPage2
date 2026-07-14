e6d90131986300c96b196c29dcd2de895c69e29a
makspyankov150796-design <maks.pyankov.150796@gmail.com>
2026-07-14 10:37:09 +0300

    1

--- frmMeterValueEditFrame.pas
+++ frmMeterValueEditFrame.pas
@@ -2491,48 +2491,6 @@ begin
   TabItemStabilityResult.Visible := True;
 end;
 
-procedure TFrameMeterValueEdit.SetIntegratedMode(const AMainParametersParent,
-  AStabilityParent: TControl);
-begin
-  TabControlMain.Visible := False;
-  TabItemMainParameters.Visible := False;
-  TabItemStabilityForecast.Visible := False;
-
-  if AMainParametersParent <> nil then
-  begin
-    if LayoutRoot.Parent <> AMainParametersParent then
-      LayoutRoot.Parent := AMainParametersParent;
-    LayoutRoot.Align := TAlignLayout.Client;
-    LayoutRoot.Visible := True;
-    LayoutRoot.Enabled := True;
-    LayoutRoot.Opacity := 1;
-    LayoutRoot.BringToFront;
-  end;
-
-  if AStabilityParent <> nil then
-  begin
-    if TabControlStability.Parent <> AStabilityParent then
-      TabControlStability.Parent := AStabilityParent;
-    TabControlStability.Align := TAlignLayout.Client;
-    TabControlStability.Visible := True;
-    TabControlStability.Enabled := True;
-    TabControlStability.Opacity := 1;
-    TabControlStability.TabPosition := TTabPosition.Top;
-
-    if LayoutConclusion.Parent <> AStabilityParent then
-      LayoutConclusion.Parent := AStabilityParent;
-    LayoutConclusion.Align := TAlignLayout.Bottom;
-    LayoutConclusion.Height := 148;
-    LayoutConclusion.Visible := True;
-    LayoutConclusion.Enabled := True;
-    LayoutConclusion.Opacity := 1;
-    LayoutConclusion.BringToFront;
-  end;
-
-  TabItemStabilityData.Visible := True;
-  TabItemStabilitySettings.Visible := True;
-  TabItemStabilityResult.Visible := True;
-end;
 
 procedure TFrameMeterValueEdit.SaveChanges;
 begin
