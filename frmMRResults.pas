9f331e268b35a7d6eb55a3e4da920a2fd0d03e61
Noxus0685 <53409785+Noxus0685@users.noreply.github.com>
2026-08-03 17:29:41 +0300

    Avoid dynamic array expression in error list

--- frmMRResults.pas
+++ frmMRResults.pas
@@ -547,30 +547,39 @@ begin
   Result := FormatDeviceError(AValue) + '%';
 end;
 
+function FormatMRActualErrorValue(const AValue: Double): string;
+begin
+  // Processing renders saved result cells with this production precision.
+  // -MaxDouble is TMeterValue's marker for an unavailable numeric value.
+  if IsNan(AValue) or IsInfinite(AValue) or (AValue <= -MaxDouble) then
+    Exit('-');
+  Result := FormatFloat('0.###', AValue);
+end;
+
 function TFrameMRResults.FormatSpillageErrors(ADevicePoint: TDevicePoint; ASpillage: TPointSpillage): string;
 var
-  ErrValues: TArray<string>;
-  I: Integer;
+  DataPoint: TPointSpillage;
 begin
   Result := '';
   if ASpillage = nil then
     Exit;
 
-  SetLength(ErrValues, 0);
-
   if (ADevicePoint <> nil) and (ADevicePoint.ProtocolDataPoints <> nil) and (ADevicePoint.ProtocolDataPoints.Count > 0) then
   begin
-    SetLength(ErrValues, ADevicePoint.ProtocolDataPoints.Count);
-    for I := 0 to ADevicePoint.ProtocolDataPoints.Count - 1 do
-      ErrValues[I] := FormatErrorValue(ADevicePoint.ProtocolDataPoints[I].Error);
+    if ADevicePoint.ProtocolDataPoints.Count = 1 then
+      Exit(FormatMRActualErrorValue(ADevicePoint.ProtocolDataPoints[0].Error));
+
+    Result := '[';
+    for DataPoint in ADevicePoint.ProtocolDataPoints do
+    begin
+      if Result <> '[' then
+        Result := Result + '; ';
+      Result := Result + FormatMRActualErrorValue(DataPoint.Error);
+    end;
+    Result := Result + ']';
   end
   else
-    ErrValues := [FormatErrorValue(ASpillage.Error)];
-
-  if Length(ErrValues) = 1 then
-    Result := ErrValues[0]
-  else
-    Result := '[' + string.Join('; ', ErrValues) + ']';
+    Result := FormatMRActualErrorValue(ASpillage.Error);
 end;
 
 function TFrameMRResults.BuildErrorsListText(ADevice: TDevice;
@@ -598,14 +607,14 @@ begin
       Continue;
 
     SetLength(Items, Cnt + 1);
-    Items[Cnt] := FormatErrorValue(S.Error);
+    Items[Cnt] := FormatMRActualErrorValue(S.Error);
     Inc(Cnt);
   end;
 
   if AIncludeCurrent then
   begin
     SetLength(Items, Cnt + 1);
-    Items[Cnt] := FormatErrorValue(ACurrentError);
+    Items[Cnt] := FormatMRActualErrorValue(ACurrentError);
     Inc(Cnt);
   end;
 
@@ -731,7 +740,7 @@ begin
 
         ErrorsText := BuildErrorsListText(Device, ASessionPoint, CurrentError, True);
         if ErrorsText = '' then
-          ErrorsText := '[' + FormatErrorValue(CurrentError) + ']';
+          ErrorsText := '[' + FormatMRActualErrorValue(CurrentError) + ']';
         Result := FormatErrorValue(DevicePoint.Error) + ' / ' + ErrorsText;
       end;
 
